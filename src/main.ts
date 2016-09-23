99b166eaa207f5d588b4ad589acde8a8927e5ef7
Anton <anton.kharitonov@sap.com>
2016-09-23 15:03:40 +0200

    try to fix webpack build on travis

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/main.d.ts" />
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import '!style!css!sass!../assets/styles/styles.scss';
